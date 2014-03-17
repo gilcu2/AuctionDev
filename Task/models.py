39aeb24d7f5026d670defa4e17b9c16004c88940
Reynaldo Gil García <gilcu2@gmail.com>
2014-03-17 10:37:39 +0100

    Initial Migration

--- Task/models.py
+++ Task/models.py
@@ -5,7 +5,7 @@ from django.db import models
 # - owner: Who propouse the task, check and paid
 # - maker: Who make the task
 # - state: Proposal, asigned, working, testing,done
-# - subtasks: A final task(without subtask) must be clear, simple and able to be make by one person.
+# - subtasks: Task needed to make this task. A final task(without subtask) must be clear, simple and able to be make by one person.
 #   If not, it must be divide by the owner or the maker, into another tasks
 # - price: Price proposal by the owner
 # - time: Time to done proposal by the owner
@@ -14,17 +14,22 @@ from django.db import models
 # - tags: tags to classify
 class Task(models.Model):
 
-    states={'Proposal','Asigned','Working','Testing','Done'}
-    title=models.CharField(max_length==50)
+    states=('Proposal','Asigned','Working','Testing','Done')
+    title=models.CharField(max_length=50)
     description=models.CharField(max_length=500)
-    state=models.IntegerField(choices=states)
-    price=models.IntegerField()
-    time=models.IntegerField()
-    owner=models.ForeignKey(User)
-    maker=models.ForeignKey(User)
-    proposals=models.ForeignKey(Proposal)
-    test=models.ForeignKey(Test)
-    tags=models.ForeignKey(Tag)
+    # state=models.IntegerField(choices=states)
+    subtasks=models.ForeignKey('self',blank=True)
+    initailPrice=models.IntegerField()
+    initialTime=models.IntegerField()
+    asignedPrice=models.IntegerField(blank=True)
+    asignedTime=models.IntegerField(blank=True)
+    finalPrice=models.IntegerField(blank=True)
+    finalTime=models.IntegerField(blank=True)
+    owner=models.ForeignKey('User',related_name='task_owner')
+    maker=models.ForeignKey('User',related_name='task_maker',blank=True)
+    proposals=models.ForeignKey('Proposal',blank=True)
+    test=models.ForeignKey('Test',blank=True)
+    tags=models.ForeignKey('Tag',blank=True)
 
 
 class User(models.Model):
@@ -39,12 +44,12 @@ class Test(models.Model):
 
     states={'ToCheck','Failed','OK'}
 
-    title=models.CharField(max_length==50)
+    title=models.CharField(max_length=50)
     description=models.CharField(max_length=500)
-    state=models.IntegerField(choices=states)
+    #state=models.IntegerField(choices=states)
 
 class Tag(models.Model):
-    title=models.CharField(max_length==50)
+    title=models.CharField(max_length=50)
     description=models.CharField(max_length=500)
     color=models.IntegerField()
 
