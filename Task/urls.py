be08b9e1c71f89c24d9fd691c239824dc5625d6b
Reynaldo Gil García <gilcu2@gmail.com>
2014-03-18 14:36:42 +0100

    First views

--- Task/urls.py
+++ Task/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import patterns, url
 from Task import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<task_id>\d+)/$', views.detail, name='detail'),
+    url(r'^(?P<task_id>\d+)/tests/$', views.tests, name='tests'),
+    url(r'^(?P<task_id>\d+)/proposals/$', views.proposals, name='proposals'),
 )
--- Task/views.py
+++ Task/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
-
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+from Task.models import *
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the Task index.")
+    latest_tasks = Task.objects.order_by('-initialDate')[:5]
+    context = {'latest_tasks': latest_tasks}
+    return render(request,'Task/index.html',context)
+
+def detail(request, task_id):
+    task=get_object_or_404(Task,pk=task_id)
+    return render(request, 'Task/detail.html', {'task': task})
+
+
+def tests(request, task_id):
+    return HttpResponse("You're looking at the test of task %s." % task_id)
+
+def proposals(request, task_id):
+    return HttpResponse("You're looking on proposals of task %s." % task_id)
