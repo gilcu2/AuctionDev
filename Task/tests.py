20ac2f37dcbb1d21925d7b1d2b83838392b9dd9e
Reynaldo Gil García <gilcu2@gmail.com>
2014-03-20 14:48:20 +0100

    css

--- Task/tests.py
+++ Task/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+
+from Task.models import *
+
+class TaskMethodTests(TestCase):
+
+    def test_proper_subtask(self):
+        """
+        A task can't be subtask of inself
+        """
+        user=User(name='testuser')
+        user.save()
+        task=Task(owner=user, initialCost=1, initialDuration=1)
+        task.save()
+        task.subtasks.add(task)
+        for ta in task.subtasks.all():
+            self.assertEqual(task==ta, False)
